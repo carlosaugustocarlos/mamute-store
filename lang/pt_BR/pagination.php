a24abd4efb11b336e8cd4ee333cf6e1ebd169e41
carlosaugustocarlos <augustoaugustocarlos4@gmail.com>
2026-06-12 14:21:14 +0100

    fix: include missing file in pagination translation fix

---
+++ lang/pt_BR/pagination.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'previous' => '&laquo; Anterior',
+    'next' => 'Seguinte &raquo;',
+    'showing' => 'Mostrando',
+    'to' => 'a',
+    'of' => 'de',
+    'results' => 'resultados',
+];
